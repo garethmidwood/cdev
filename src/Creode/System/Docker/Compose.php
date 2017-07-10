9a2f63a0a2796673237b2cdf6d1e254e0f83022d
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-07-10 17:05:49 +0100

    Docker sync and compose now only run if the config files exist

--- src/Creode/System/Docker/Compose.php
+++ src/Creode/System/Docker/Compose.php
@@ -6,30 +6,57 @@ use Creode\System\Command;
 class Compose extends Command
 {
     const COMMAND = 'docker-compose';
+    const FILE = 'docker-compose.yml';
+
+    /**
+     * @var boolean
+     */
+    private $_configExists = false;
+
+    public function __construct() 
+    {
+        $this->_configExists = file_exists(self::FILE);
+    }
 
     public function up($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+
         $this->run(self::COMMAND, ['up'], $path);
 
-        return 'docker-compose up completed';
+        return self::COMMAND . ' up completed';
     }
 
     public function stop($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+        
         $this->run(self::COMMAND, ['stop'], $path);
 
-        return 'docker-compose stop completed';
+        return self::COMMAND . ' stop completed';
     }
 
     public function rm($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+        
         $this->run(self::COMMAND, ['rm', '-f'], $path);
 
-        return 'docker-compose rm completed';
+        return self::COMMAND . ' rm completed';
     }
 
     public function runCmd($path, $command, $options)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+        
         $this->run(self::COMMAND, $options, $path);        
     }
 }
--- src/Creode/System/Docker/Sync.php
+++ src/Creode/System/Docker/Sync.php
@@ -6,32 +6,59 @@ use Creode\System\Command;
 class Sync extends Command
 {
     const COMMAND = 'docker-sync';
+    const FILE = 'docker-sync.yml';
+
+    /**
+     * @var boolean
+     */
+    private $_configExists = false;
+
+    public function __construct() 
+    {
+        $this->_configExists = file_exists(self::FILE);
+    }
     
     public function start($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+
         $this->run(self::COMMAND, ['start'], $path);
 
-        return 'docker-sync start completed';
+        return self::COMMAND . ' start completed';
     }
 
     public function stop($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+
         $this->run(self::COMMAND, ['stop'], $path);
 
-        return 'docker-sync stop completed';
+        return self::COMMAND . ' stop completed';
     }
 
     public function clean($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+
         $this->run(self::COMMAND, ['clean'], $path);
 
-        return 'docker-sync clean completed';
+        return self::COMMAND . ' clean completed';
     }
 
     public function sync($path)
     {
+        if (!$this->_configExists) {
+            return self::FILE . ' not found.';
+        }
+
         $this->run(self::COMMAND, ['sync'], $path);
 
-        return 'docker-sync sync completed';
+        return self::COMMAND . ' sync completed';
     }
 }
