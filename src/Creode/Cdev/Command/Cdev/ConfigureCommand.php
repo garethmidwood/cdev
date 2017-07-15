d4b1d0e62ae69a018c675119323775b2baedf93a
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-07-15 17:59:36 +0100

    Add code src directory to configure command

--- src/Creode/Cdev/Command/Cdev/ConfigureCommand.php
+++ src/Creode/Cdev/Command/Cdev/ConfigureCommand.php
@@ -11,6 +11,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
 class ConfigureCommand extends Command
@@ -18,6 +20,9 @@ class ConfigureCommand extends Command
     private $_config = array(
         'version' => '2',
         'config' => array(
+            'dir' => array(
+                'src' => null
+            ),
             'environment' => array(
                 'type' => 'docker',
                 'framework' => null
@@ -34,10 +39,37 @@ class ConfigureCommand extends Command
         )
     );
 
+    /**
+     * @var Filesystem
+     */
+    protected $_fs;
+
+    /**
+     * @var Finder
+     */
+    protected $_finder;
+
+    /**
+     * Constructor
+     * @param Filesystem $fs
+     * @param Finder $finder
+     * @return null
+     */
+    public function __construct(
+        Filesystem $fs,
+        Finder $finder
+    ) {
+        $this->_fs = $fs;
+        $this->_finder = $finder;
+
+        parent::__construct();
+    }
+
+
     protected function configure()
     {
         $this->setName('configure');
-        $this->setDescription('Creates this environments configuration file');
+        $this->setDescription('Configures this repository for its environment and framework');
 
         $this->addOption(
             'path',
@@ -56,39 +88,66 @@ class ConfigureCommand extends Command
         );
     }
 
+    /**
+     * Executes the command
+     * @param InputInterface $input 
+     * @param OutputInterface $output 
+     * @return null
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $configDir = $input->getOption('path') . '/' . Config::CONFIG_DIR;
+        $this->_input = $input;
+        $this->_output = $output;
 
-        if (!file_exists($configDir)) {
-            mkdir($configDir, 0744);
-        }
-
-        $configFile = $configDir . $input->getOption('config');
+        $path = $this->_input->getOption('path');
+        $configDir = $path . '/' . Config::CONFIG_DIR;
+        $configFile = $configDir . $this->_input->getOption('config');
         $servicesFile = $configDir . 'services.env.xml';
 
         if (file_exists($configFile)) {
             $this->_config = Yaml::parse(file_get_contents($configFile));
         }
 
-        $answers = $this->askQuestions($input, $output);
+        $answers = $this->askQuestions();
 
-        $output->writeln('Writing config file to ' . $configFile);
+        $this->_output->writeln('Writing config file to ' . $configFile);
+        $this->saveConfigFile($configFile);
 
-        $configuration = Yaml::dump($this->_config);
-
-        file_put_contents(
-            $configFile,
-            $configuration
-        );
-
+        $this->_output->writeln('Writing services file to ' . $servicesFile);
         $this->saveServicesXml($servicesFile);
     }
 
-    private function askQuestions(InputInterface $input, OutputInterface $output)
+    /**
+     * Asks the user questions
+     * @return null
+     */
+    private function askQuestions()
     {
         $helper = $this->getHelper('question');
+        $path = $this->_input->getOption('path');
 
+        /**
+         * 
+         * DIRECTORY STRUCTURE
+         * 
+         */
+        $originalSrc = $this->_config['config']['dir']['src'];
+
+        $this->askQuestion(
+            'Code directory',
+            $this->_config['config']['dir']['src']
+        );
+
+        if ($this->_config['config']['dir']['src'] != $originalSrc) {
+            $this->changeSrcDir($originalSrc, $this->_config['config']['dir']['src']);
+        }
+
+
+        /**
+         * 
+         * ENVIRONMENTAL / FRAMEWORK
+         * 
+         */
         $question = new ChoiceQuestion(
             'Environment type',
             array(
@@ -97,81 +156,75 @@ class ConfigureCommand extends Command
             )
         );
         $question->setErrorMessage('Environment type %s is invalid.');
-        $this->_config['config']['environment']['type'] = $helper->ask($input, $output, $question);
+        $this->_config['config']['environment']['type'] = $helper->ask($this->_input, $this->_output, $question);
+
 
         $question = new ChoiceQuestion(
             'Framework',
             // TODO: Find a better way to include these, ideally by adding the classes somehow
             array(
-                'magento1',
-                'magento2',
-                'drupal7',
-                'drupal8',
-                'wordpress'
+                'magento1', // Framework\Magento1\Magento1::NAME,
+                'magento2', // Framework\Magento2\Magento2::NAME,
+                'drupal7',  // Framework\Drupal7\Drupal7::NAME,
+                'drupal8',  // Framework\Drupal8\Drupal8::NAME,
+                'wordpress' // Framework\WordPress\WordPress::NAME,
             )
-            // array(
-            //     Framework\Magento1\Magento1::NAME,
-            //     Framework\Magento2\Magento2::NAME,
-            //     Framework\Drupal7\Drupal7::NAME,
-            //     Framework\Drupal8\Drupal8::NAME,
-            //     Framework\WordPress\WordPress::NAME
-            // )
         );
         $question->setErrorMessage('Framework %s is invalid.');
-        $this->_config['config']['environment']['framework'] = $helper->ask($input, $output, $question);
+        $this->_config['config']['environment']['framework'] = $helper->ask($this->_input, $this->_output, $question);
 
+
+        /**
+         * 
+         * BACKUPS
+         * 
+         */
         $this->askQuestion(
             'Backups: Host',
-            $this->_config['config']['backups']['host'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['host']
         );
 
         $this->askQuestion(
             'Backups: Port',
-            $this->_config['config']['backups']['port'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['port']
         );
 
         $this->askQuestion(
             'Backups: User',
-            $this->_config['config']['backups']['user'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['user']
         );
 
         $this->askQuestion(
             'Backups: DB Directory',
-            $this->_config['config']['backups']['db-dir'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['db-dir']
         );
 
         $this->askQuestion(
             'Backups: DB file name',
-            $this->_config['config']['backups']['db-file'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['db-file']
         );
 
         $this->askQuestion(
             'Backups: Media Directory',
-            $this->_config['config']['backups']['media-dir'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['media-dir']
         );
 
         $this->askQuestion(
             'Backups: Media file name',
-            $this->_config['config']['backups']['media-file'],
-            $input, 
-            $output
+            $this->_config['config']['backups']['media-file']
         );
     }
 
-    private function askQuestion($text, &$config, $input, $output) 
-    {
+    /**
+     * Convenience method for setting config based on results of questions
+     * @param string $text 
+     * @param string &$config Current config value
+     * @return null
+     */
+    private function askQuestion(
+        $text,
+        &$config
+    ) {
         $helper = $this->getHelper('question');
 
         $question = new Question(
@@ -179,7 +232,28 @@ class ConfigureCommand extends Command
             $config
         );
 
-        $config = $helper->ask($input, $output, $question);
+        $config = $helper->ask($this->_input, $this->_output, $question);
+    }
+
+    /**
+     * Saves project specific config file
+     * @param string $configFile 
+     * @return null
+     */
+    private function saveConfigFile($configFile)
+    {
+        $configDir = dirname($configFile);
+
+        if (!file_exists($configDir)) {
+            mkdir($configDir, 0744);
+        }
+
+        $configuration = Yaml::dump($this->_config);
+
+        file_put_contents(
+            $configFile,
+            $configuration
+        );
     }
 
     /**
@@ -189,6 +263,12 @@ class ConfigureCommand extends Command
      */
     private function saveServicesXml($servicesFile)
     {
+        $configDir = dirname($servicesFile);
+        
+        if (!file_exists($configDir)) {
+            mkdir($configDir, 0744);
+        }
+
         $servicesContent = file_get_contents(__DIR__ . '/../../../../templates/services.env.xml');
         
         $searches = [
@@ -209,5 +289,107 @@ class ConfigureCommand extends Command
         );
     }
 
+    /**
+     * Changes (or sets up) the src directory for the site code
+     * @param string|null $oldSrc 
+     * @param string $newSrc 
+     * @return null
+     */
+    private function changeSrcDir($oldSrc = null, $newSrc)
+    {
+        if (isset($oldSrc)) 
+        {
+            $this->renameSrcDir($oldSrc, $newSrc);
+        } else {
+            $this->createSrcDir($newSrc);
+            $this->moveFilesToSrc($newSrc);
+        }
+    }
+
+    /**
+     * Renames an existing src directory
+     * @param string $oldSrc 
+     * @param string $src 
+     * @return null
+     */
+    private function renameSrcDir($oldSrc, $src)
+    {
+        $this->_output->writeln('===== Renaming src directory');
+
+        $path = $this->_input->getOption('path');
+
+        $oldSrcPath = $path . '/' . $oldSrc;
+        $srcPath = $path . '/' . $src;
+
+        if (!$this->_fs->exists($oldSrcPath))
+        {
+            $this->_output->writeln("$oldSrc directory doesn't exist. Aborting");
+            throw new \Exception("$oldSrc directory doesn't exist");
+        }
+            
+        $this->_output->writeln("Renaming $oldSrc directory to $src");
+
+        $this->_fs->rename(
+            $oldSrcPath,
+            $srcPath 
+        );
+    }
+
+    /**
+     * Creates src directory
+     * @param string $src 
+     * @return null
+     */
+    private function createSrcDir($src)
+    {
+        $this->_output->writeln('===== Creating src directory');
+
+        $path = $this->_input->getOption('path');
+
+        $srcPath = $path . '/' . $src;
+
+        if ($this->_fs->exists($srcPath))
+        {
+            $this->_output->writeln("$src directory already exists. Continuing with existing dir");
+            return;
+        }
+            
+        $this->_output->writeln("Creating $src directory");
+        
+        $this->_fs->mkdir($srcPath, 0740);
+
+        $this->_output->writeln("$src directory created");
+    }
+
+    /**
+     * Moves files into the src directory
+     * @param string $src 
+     * @return null
+     */
+    private function moveFilesToSrc($src)
+    {
+        $this->_output->writeln('===== Moving files to src directory');
+
+        $path = $this->_input->getOption('path');
+
+        $this->_finder
+            ->in($path)
+            ->depth('== 0')
+            ->exclude($src);
+
+        if (count($this->_finder) == 0) {
+            $this->_output->writeln("No files to move");
+            return;
+        }
+
+        foreach ($this->_finder as $file) {
+            $this->_output->writeln("Moving {$file->getFileName()} into $src directory");
+
+            $this->_fs->rename(
+                $file->getPath() . '/' . $file->getFileName(),
+                $file->getPath() . '/' . $src . '/' . $file->getFileName() 
+            );
+        }
+    }
 
 }
