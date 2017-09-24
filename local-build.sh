c1269b9b09f002d29f8dca54629eb87d5ba732ff
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-09-24 11:40:06 +0100

    Update local build to use same build methods as a release

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# credit to: https://moquet.net/blog/distributing-php-cli/ 
+INSTALL_DIR=~/.cdev
 
+ICON_COMPLETE_COLOUR=`tput setaf 2`
+NO_COLOUR=`tput sgr0`
+
+ICON_COMPLETE="${ICON_COMPLETE_COLOUR}\xcf\xbe${NO_COLOUR}"
+
+TARGET_RELEASE_PATH="${INSTALL_DIR}/cdev-local.phar"
+TARGET_RELEASE_KEY_PATH="${INSTALL_DIR}/cdev-local.phar.pubkey"
+
+ALIAS='/usr/local/bin/cdev-local'
+
+box key:create
 box build
 
-mv cdev.phar /usr/local/bin/cdev-local
+mv cdev.phar $TARGET_RELEASE_PATH
+mv cdev.phar.pubkey $TARGET_RELEASE_KEY_PATH
+
+rm $ALIAS
+ln -s $TARGET_RELEASE_PATH $ALIAS
