3ca3a1f03a5917d331cfd85415a9ddd34d152fc8
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-10-22 12:32:58 +0100

    Removed commented out frameworks from collection

--- src/Creode/Collections/FrameworkCollection.php
+++ src/Creode/Collections/FrameworkCollection.php
@@ -9,10 +9,5 @@ class FrameworkCollection extends Collection
         $this->addItem('\Creode\Framework\Custom\Custom');
 
         \Creode\Cdev\Plugin\Manager::registerFrameworks($this);
-        // $this->addItem('\Creode\Framework\Magento1\Magento1');
-        // $this->addItem('\Creode\Framework\Magento2\Magento2');
-        // $this->addItem('\Creode\Framework\Drupal7\Drupal7');
-        // $this->addItem('\Creode\Framework\Drupal8\Drupal8');
-        // $this->addItem('\Creode\Framework\WordPress\WordPress');
     }
 }
