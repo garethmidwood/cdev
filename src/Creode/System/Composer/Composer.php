1d3e7b09ec991bfdc4e4729455f8e82dc34a98ae
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-10-31 22:37:36 +0000

    Add plugin search command

--- src/Creode/System/Composer/Composer.php
+++ src/Creode/System/Composer/Composer.php
@@ -81,6 +81,27 @@ class Composer extends Command
         );
 
         return 'composer require ' . implode(' ', $package) . ' completed';
+    }    
+
+    /**
+     * Searches packages
+     * @param string $path 
+     * @param string|array $package 
+     * @param array $parameters
+     * @return string
+     */
+    public function search($path, $package, $parameters = [])
+    {
+        $packages = is_array($package) ? $package : [$package];
+        $params = array_merge(['search'], $packages, $parameters);
+
+        $this->runExternalCommand(
+            $this->_composer,
+            $params,
+            $path
+        );
+
+        return 'composer search ' . implode(' ', $params) . ' completed';
     }
 
     /**
